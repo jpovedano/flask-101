4f0bc9ee32e452e9aec0ce5eab4aabd4ac347e2b
Javier Povedano <javier@rti.com>
2019-11-12 00:30:10 +0100

    Add restplus

--- hello.py
+++ hello.py
@@ -1,25 +1,20 @@
 import flask
-from flask import render_template, send_file
+from flask_restplus import Resource, Api
 
 app = flask.Flask(__name__)
+api = Api(app)
 
-@app.route('/static/<path:path>')
-def get_static_file(path):
-    return send_file(path)
+USERS = [
+    {'id': 1, 'name': 'alice', 'email': 'alice@doe'},
+    {'id': 2, 'name': 'bob', 'email': 'bob@doe'},
+    {'id': 3, 'name': 'charlie', 'email': 'charlie@doe'}
+]
 
-@app.route('/users/<int:id>')
-def get_user(id):
-    users = [{"id": id, "name": "john", "email": "john@doe"}]
-    return render_template('template.html', users=users)
+@api.route('/users')
+class UserList(Resource):
 
-@app.route('/users')
-def get_users():
-    users = [
-        {"id": 1, "name": "alice", "email": "alice@doe", "enabled": True},
-        {"id": 2, "name": "bob", "email": "bob@doe", "enabled": True},
-        {"id": 3, "name": "charlie", "email": "charlie@doe", "enabled": False},
-    ]
-    return render_template('template.html', users=users)
+    def get(self):
+        return USERS
 
 if __name__ == '__main__':
     app.run()
