f8dda0419950d1e3baf5ba4b56f781ba7bf02e98
Javier Povedano <javier@rti.com>
2019-11-11 23:09:13 +0100

    template from file

--- hello.py
+++ hello.py
@@ -1,19 +1,12 @@
 import flask
-from flask import render_template_string
+from flask import render_template
 
 app = flask.Flask(__name__)
 
-template="""
-<html>
-<body>
-<h1>Hello User {{ id }}</h1>
-</body>
-</html>
-"""
-
 @app.route('/user/<int:id>')
 def get_user(id):
-    return render_template_string(template, id=id)
+    user = {"id": id, "name": "john", "email": "john@doe"}
+    return render_template('template.html', user=user)
 
 if __name__ == '__main__':
     app.run()
