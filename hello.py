8950b24a16c3293164a9abcb32ef6a62ac9d7f18
Javier Povedano <javier@rti.com>
2019-11-11 23:34:35 +0100

    get list of enabled users

--- hello.py
+++ hello.py
@@ -3,10 +3,19 @@ from flask import render_template
 
 app = flask.Flask(__name__)
 
-@app.route('/user/<int:id>')
+@app.route('/users/<int:id>')
 def get_user(id):
-    user = {"id": id, "name": "john", "email": "john@doe"}
-    return render_template('template.html', user=user)
+    users = [{"id": id, "name": "john", "email": "john@doe"}]
+    return render_template('template.html', users=users)
+
+@app.route('/users')
+def get_users():
+    users = [
+        {"id": 1, "name": "alice", "email": "alice@doe", "enabled": True},
+        {"id": 2, "name": "bob", "email": "bob@doe", "enabled": True},
+        {"id": 3, "name": "charlie", "email": "charlie@doe", "enabled": False},
+    ]
+    return render_template('template.html', users=users)
 
 if __name__ == '__main__':
     app.run()
