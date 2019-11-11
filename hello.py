56f7ed68d28cd02d884063c4c068bf477f5e54ef
Javier Povedano <javier@rti.com>
2019-11-11 22:39:47 +0100

    add multiple routes

--- hello.py
+++ hello.py
@@ -6,5 +6,13 @@ app = flask.Flask(__name__)
 def hello():
     return "Hello World"
 
+@app.route('/about')
+def about():
+    return "About"
+
+@app.route('/user/<int:id>')
+def get_user(id):
+    return "Hello user #{userid}".format(userid=id)
+
 if __name__ == '__main__':
     app.run()
