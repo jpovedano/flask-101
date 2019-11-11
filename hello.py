545c76ecf620cd9de0b8f38390e7800a7323986e
Javier Povedano <javier@rti.com>
2019-11-12 00:43:17 +0100

    Add model

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import flask
-from flask_restplus import Resource, Api
+from flask_restplus import Resource, Api, fields
 
 app = flask.Flask(__name__)
 api = Api(app)
@@ -10,9 +10,19 @@ USERS = [
     {'id': 3, 'name': 'charlie', 'email': 'charlie@doe'}
 ]
 
+USERMODEL = api.model(
+    name='User',
+    model={
+        'id': fields.Integer(required=True),
+        'name': fields.String(required=True, example="John Doe"),
+        'email': fields.String(example="john@doe")
+    }
+)
+
 @api.route('/users')
 class UserList(Resource):
 
+    @api.marshal_list_with(USERMODEL)
     def get(self):
         return USERS
 
