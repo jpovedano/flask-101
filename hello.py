141dd9bbdb73ae9fd0ebceec10b02602727ebfee
Javier Povedano <javier@rti.com>
2019-11-12 01:20:56 +0100

    Add endppoints

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import flask
 from flask_restplus import Resource, Api, fields
 
 app = flask.Flask(__name__)
-api = Api(app)
+api = Api(app, title="User Database API")
 
 USERS = [
     {'id': 1, 'name': 'alice', 'email': 'alice@doe'},
@@ -23,8 +23,34 @@ USERMODEL = api.model(
 class UserList(Resource):
 
     @api.marshal_list_with(USERMODEL)
+    @api.response(200, 'Sucess')
     def get(self):
+        """Get the list of users."""
         return USERS
+
+    @api.expect(USERMODEL, validate=True)
+    @api.response(200, 'Sucess')
+    @api.response(303, 'Already existing')
+    def post(self):
+        """Add a new user."""
+        payload = flask.request.get_json()
+        for user in USERS:
+            if user['id'] == payload['id']:
+                return {"message": "Record already existed"}, 303
+        USERS.append(payload)
+        return {"message": "Record updated"}, 200
+
+@api.route('/users/<int:id>')
+class User(Resource):
+    @api.marshal_with(USERMODEL)
+    @api.response(200, 'Success')
+    @api.response(404, 'User not found')
+    def get(self, id):
+        for user in USERS:
+            if user['id'] == id:
+                return user
+        # If not found, raise a 404
+        flask.abort(404)
 
 if __name__ == '__main__':
     app.run()
